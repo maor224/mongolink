2d020b3a8219df50dc6e5dae79ffc99df0c42468
maor224 <maorbar2204@gmail.com>
2024-08-20 23:04:51 +0300

    Update connection functionality

--- src/common/mongo_connection.py
+++ src/common/mongo_connection.py
@@ -1,26 +1,29 @@
-from motor.motor_asyncio import (
-    AsyncIOMotorClient,
-    AsyncIOMotorCollection,
-    AsyncIOMotorDatabase,
-)
+import asyncio
+
+from motor.motor_asyncio import AsyncIOMotorClient, AsyncIOMotorDatabase
 
 from src.config.settings.connection import ConnectionSettings
 
 
 class MongoDBConnection:
-
     def __init__(self):
         self.settings = ConnectionSettings()
-        self.client = AsyncIOMotorClient(self.get_connection_string())
+        self.client = None
 
-    def get_connection_string(self) -> str:
-        return f"mongodb://{self.settings.username}:{self.settings.password}@{self.settings.mongodb_host}:{self.settings.port}"
+    async def initialize_client(self):
+        connection_string = await self.get_connection_string()
+        self.client = AsyncIOMotorClient(connection_string)
 
-    def get_database(self, database_name: str) -> AsyncIOMotorDatabase:
+    async def get_connection_string(self) -> str:
+        return f"mongodb+srv://{self.settings.username}:{self.settings.password}@{self.settings.host}"
+
+    async def get_database(self, database_name: str) -> AsyncIOMotorDatabase:
+        if not self.client:
+            raise ValueError(
+                "Client is not initialized. Call `initialize_client` first."
+            )
         return self.client[database_name]
 
-    def get_collection(
-        self, database_name: str, collection_name: str
-    ) -> AsyncIOMotorCollection:
-        database = self.get_database(database_name)
-        return database[collection_name]
+    async def create_collection(self, database_name: str, collection_name: str) -> None:
+        database = await self.get_database(database_name)
+        await database.create_collection(collection_name)
